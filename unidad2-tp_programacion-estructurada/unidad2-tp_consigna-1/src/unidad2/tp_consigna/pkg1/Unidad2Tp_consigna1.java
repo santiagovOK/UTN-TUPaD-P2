436f5a00384ab5d90392f955b0ecac99c9e81248
Santiago Varela <121682758+santiagovOK@users.noreply.github.com>
2025-08-12 18:26:32 -0300

    Resolucion de unidad2-tp_programacion-estructurada hasta consigna 1 2025-08-12

--- unidad2-tp_programacion-estructurada/unidad2-tp_consigna-1/src/unidad2/tp_consigna/pkg1/Unidad2Tp_consigna1.java
+++ unidad2-tp_programacion-estructurada/unidad2-tp_consigna-1/src/unidad2/tp_consigna/pkg1/Unidad2Tp_consigna1.java
@@ -4,6 +4,7 @@
  */
 package unidad2.tp_consigna.pkg1;
 
+import java.util.Scanner;
 /**
  *
  * @author san
@@ -14,6 +15,41 @@ public class Unidad2Tp_consigna1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        // Creamos el objeto Scanner para poder leer posteriormente las entrasdas del usuario
+        Scanner input = new Scanner(System.in);
+        
+        // Imprimimos mensaje previo al `input` del usuario indicando que ingrese el año
+        System.out.println("Ingresá un año para que verifiquemos si es bisiesto o no: ");
+        
+        // Inicializamos la variable `anio` y le asignamos el valor ingresado por el usuario con `input.nextInt()`  
+        int anio = input.nextInt();
+        
+        // Inicializamos la variable `esBisiesto` tipo boolean en falso para luego convalidar si el año es bisiesto o no.
+        
+        boolean esBisiesto = false;
+       
+        // Estructura condicional para evaluar si el año es bisiesto o no, en función de las diferentes condiciones para considerar un año como bisiesto
+        if (anio % 4 == 0) {
+            if (anio % 100 != 0) {
+                esBisiesto = true;
+            } else if (anio % 400 == 0) {
+                esBisiesto = true;
+            } else {
+                esBisiesto = false;
+            }
+            
+        // Si directamente no es divisible por 4, `esBisiesto` será falso  
+        } else {
+            esBisiesto = false;
+        }
+        
+        // Creamos otra estructura condicional para imprimir un mensaje al usuario en función de si `esBisiesto` o no
+        
+        if (esBisiesto) {
+            System.out.println("El año " + anio + " es bisiesto.");
+        } else {
+            System.out.println("El año " + anio + " no es bisiesto.");
+        }
         
     }
     
