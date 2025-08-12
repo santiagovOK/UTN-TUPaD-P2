46f5b51a29abc94d004113c67f2e514363b14f13
Santiago Varela <121682758+santiagovOK@users.noreply.github.com>
2025-08-12 16:20:25 -0300

    Iniciando el repositorio

---
+++ unidad2-tp_programacion-estructurada/unidad2-tp_consigna-1/src/unidad2/tp_consigna/pkg1/Unidad2Tp_consigna1.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package unidad2.tp_consigna.pkg1;
+
+/**
+ *
+ * @author san
+ */
+public class Unidad2Tp_consigna1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+    
+}
