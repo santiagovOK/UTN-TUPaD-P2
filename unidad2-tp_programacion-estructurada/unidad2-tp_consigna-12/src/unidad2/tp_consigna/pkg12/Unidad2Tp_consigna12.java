1fc49ef248eb7fc94d0b16ba5d574a68b9e97c1c
Santiago Varela <121682758+santiagovOK@users.noreply.github.com>
2025-08-14 11:37:49 -0300

    Resolucion de unidad2-tp_programacion-estructurada hasta consigna 13 2025-08-14

--- unidad2-tp_programacion-estructurada/unidad2-tp_consigna-12/src/unidad2/tp_consigna/pkg12/Unidad2Tp_consigna12.java
+++ unidad2-tp_programacion-estructurada/unidad2-tp_consigna-12/src/unidad2/tp_consigna/pkg12/Unidad2Tp_consigna12.java
@@ -4,6 +4,8 @@
  */
 package unidad2.tp_consigna.pkg12;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Santiago Octavio Varela / @santiagovOK (GitHub)
@@ -15,7 +17,32 @@ public class Unidad2Tp_consigna12 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // Creamos el objeto Scanner para poder leer posteriormente las entrasdas del usuario
+        Scanner input = new Scanner(System.in);
+        
+        
+        // Asignamos a la variable precios un array de `[5]` posiciones, agregando los diferentes valores en la misma línea
+        double [] precios = {199.99, 299.5, 149.75, 399.0, 89.99};
+        
+        // Imprimimos el array de "precios originales" usando `for`, que recorre cada una de las posiciones hasta llegar a la última inclusive
+        
+        System.out.println("Precios originales: "); // Impreso por fuera del `for` para que no se repita
+        for (int i = 0; i < precios.length; i++) {
+            double precio = precios[i];
+            System.out.println("$" + precio);
+            
+        }
+        
+        // Modificamos el precio de la posición 2 del array, que sería el tercero en la lista
+        precios[2] = 129.99; 
+        
+        // Imprimimos el array de "precios modificados" usando `for`, que recorre cada una de las posiciones hasta llegar a la última inclusive
+        System.out.println("\nPrecios modificados: "); // Impreso por fuera del `for` para que no se repita
+        for (int i = 0; i < precios.length; i++) {
+            double precio = precios[i];
+            System.out.println("$" + precio);
+        }
+        
     }
     
 }
--- unidad2-tp_programacion-estructurada/unidad2-tp_consigna-13/src/unidad2/tp_consigna/pkg13/Unidad2Tp_consigna13.java
+++ unidad2-tp_programacion-estructurada/unidad2-tp_consigna-13/src/unidad2/tp_consigna/pkg13/Unidad2Tp_consigna13.java
@@ -4,6 +4,8 @@
  */
 package unidad2.tp_consigna.pkg13;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Santiago Octavio Varela / @santiagovOK (GitHub)
@@ -14,8 +16,43 @@ public class Unidad2Tp_consigna13 {
     /**
      * @param args the command line arguments
      */
+    
+    // Creamos la función recursiva para que se imprima recursivamente cada uno de los elementos del array
+    public static void mostrarPreciosRecursivo(double[] array, int indice) {
+        
+        // Establecemos el caso base con una estructura condicional
+        // Si el índice alcanza la longitud del array, terminamos la recursión
+        
+        if (indice >= array.length) {
+            return;
+        }
+        
+        // Imprimimos el precio en la posición actual
+        System.out.println("$" + array[indice]);
+        
+        // Hacemos la llamada recursiva, luego incrementamos el índice para procesar el siguiente elemento
+        mostrarPreciosRecursivo(array, indice + 1);
+    }
     public static void main(String[] args) {
-        // TODO code application logic here
+        // Creamos el objeto Scanner para poder leer posteriormente las entradas del usuario
+        Scanner input = new Scanner(System.in);
+        
+        // Declaramos e inicializamos un array con precios de productos
+        double[] precios = {199.99, 299.5, 149.75, 399.0, 89.99};
+        
+        // Mostramos los precios originales usando función recursiva creada previamente
+        System.out.println("Precios originales:");
+        mostrarPreciosRecursivo(precios, 0);
+        
+        // Modificamos el precio de la posición 2 del array, que sería el tercero en la lista
+        // (igual que en el ejercicio anterior)
+        precios[2] = 129.99;
+        
+        // Mostramos los precios modificados usando función recursiva
+        // No es necesaria "otra función recursiva", simplemente usamos la misma
+        System.out.println("\nPrecios modificados:");
+        mostrarPreciosRecursivo(precios, 0);
+        
     }
     
 }
