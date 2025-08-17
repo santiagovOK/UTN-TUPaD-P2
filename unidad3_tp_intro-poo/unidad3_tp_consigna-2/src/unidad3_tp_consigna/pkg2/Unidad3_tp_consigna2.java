17ac2e4ca41fe1909d284aa4c7677e7618bf3985
Santiago Varela <121682758+santiagovOK@users.noreply.github.com>
2025-08-17 17:07:48 -0300

    Creando archivos iniciales para unidad3_tp_intro-poo

---
+++ unidad3_tp_intro-poo/unidad3_tp_consigna-2/src/unidad3_tp_consigna/pkg2/Unidad3_tp_consigna2.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package unidad3_tp_consigna.pkg2;
+
+/**
+ *
+ * @author Santiago Octavio Varela / @santiagovOK (GitHub)
+ * <santiago.varela@tupad.utn.edu.ar>
+ */
+public class Unidad3_tp_consigna2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
